1ab32df5e7cb2b216f44e3d2a65703e6c9f11ea6
abdsh55 <abdullahsherif73+git@gmail.com>
2024-03-04 16:42:02 +0200

    Initialize FIrebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  await _initCrashlytics();
+
   runApp(const App());
 }
 
-
+Future<void> _initCrashlytics() async {
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  foundation.PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
