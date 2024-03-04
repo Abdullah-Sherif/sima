70d1935598a912d00f7cfcf458db1d49003286f8
abdsh55 <abdullahsherif73+git@gmail.com>
2024-03-04 17:28:12 +0200

    Create AppTheme & Colors

---
+++ lib/core/style/custom_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Colors.white,
+      offWhite = Color.fromRGBO(245, 245, 245, 1),
+      black = Colors.black,
+      almostBlack = Color(0xff040B13),
+      veryDarkGrey = Color(0xFF212121),
+      darkGrey = Color(0xFF30302F),
+      charcoal = Color.fromRGBO(77, 77, 77, 1),
+      grey = Colors.grey,
+      lightGrey = Color(0xFFBABABA),
+      veryLightGrey = Color(0xFFCACACA),
+      lightBlue = Color.fromRGBO(0, 174, 239, 1),
+      blue = Colors.blue,
+      darkBlue = Color(0xFF0277BD),
+      transparent = Colors.transparent,
+      red = Colors.red,
+      green = Colors.green;
+}
---
+++ lib/core/style/theme.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'custom_colors.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: CustomColors.offWhite,
+      fontFamily: GoogleFonts.ubuntu().fontFamily,
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        color: Colors.transparent,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        fillColor: CustomColors.white,
+        hintStyle: TextStyle(
+          color: CustomColors.charcoal,
+        ),
+      ),
+      textButtonTheme: const TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll(
+            CustomColors.charcoal,
+          ),
+          textStyle: MaterialStatePropertyAll(
+            TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: CustomColors.white),
+          ),
+        ),
+      ),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.w600,
+          height: 1,
+        ),
+        titleMedium: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          height: 1.5,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 16,
+        ),
+        bodySmall: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        labelMedium: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          height: 1.175,
+        ),
+        labelSmall: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      colorScheme: ColorScheme.fromSwatch(
+        brightness: Brightness.light,
+        primarySwatch: Colors.grey,
+        accentColor: CustomColors.charcoal,
+      ).copyWith(error: const Color(0xFFFF0000)),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: CustomColors.veryDarkGrey,
+      fontFamily: GoogleFonts.ubuntu().fontFamily,
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        color: Colors.transparent,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        fillColor: CustomColors.white,
+        hintStyle: TextStyle(
+          color: CustomColors.charcoal,
+        ),
+      ),
+      textButtonTheme: const TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll(
+            CustomColors.lightBlue,
+          ),
+          textStyle: MaterialStatePropertyAll(
+            TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: CustomColors.white),
+          ),
+        ),
+      ),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.w600,
+          height: 1,
+        ),
+        titleMedium: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          height: 1.5,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 16,
+        ),
+        bodySmall: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        labelMedium: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          height: 1.175,
+        ),
+        labelSmall: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      colorScheme: ColorScheme.fromSwatch(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.grey,
+        accentColor: CustomColors.lightBlue,
+      ).copyWith(error: const Color(0xFFFF0000)),
+    );
+  }
+}
